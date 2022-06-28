0f4845058a3f340756311821c1ca6f2e47dc654d
rajpranit <rajpranit1207@gmail.com>
2022-06-28 19:30:50 +0530

    Step-2 Column Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text("Hello World!",),);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text(""),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text("The Question !"),
+              ElevatedButton(
+                child: Text("Answer 1"),
+                onPressed: null,
+              ),
+              ElevatedButton(
+                child: Text("Answer 2"),
+                onPressed: null,
+              ),
+              ElevatedButton(
+                child: Text("Answer 3"),
+                onPressed: null,
+              )
+            ],
+          )),
+    );
   }
 }
