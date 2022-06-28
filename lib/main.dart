82f8589489f49ca448b49efd15c9e0b63b2ac644
rajpranit <rajpranit1207@gmail.com>
2022-06-28 20:43:02 +0530

    Step-3 Statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,50 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion(){
+    setState( () {
+       questionIndex += 1;
+       print(questionIndex);
+    });
+
+
+  }
+
+  var questions = [
+    'What is your favourite color?',
+    'what\'s your favourite ANIMAL? '
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: const Text(""),
+            title: Text("QUIZ TIME"),
           ),
           body: Column(
             children: <Widget>[
-              Text("The Question !"),
+              Text(questions[questionIndex]),
               ElevatedButton(
                 child: Text("Answer 1"),
-                onPressed: null,
+                onPressed: answerQuestion,
               ),
               ElevatedButton(
                 child: Text("Answer 2"),
-                onPressed: null,
+                onPressed: () => {print("Answer")},
               ),
               ElevatedButton(
                 child: Text("Answer 3"),
-                onPressed: null,
+                onPressed: answerQuestion,
               )
             ],
           )),
